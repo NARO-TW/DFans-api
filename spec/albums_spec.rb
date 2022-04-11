3b9c4b88ed61b7f36b48a1a65d109b303f606677
Huang Chuan <river1440>
2022-04-11 23:01:39 +0800

    fix error

--- spec/albums_spec.rb
+++ spec/albums_spec.rb
@@ -21,8 +21,8 @@ describe 'Test Album Handling' do
   end
 
   it 'HAPPY: should be able to get details of a single album' do
-    existing_ablum = DATA[:albums][1]
-    DFans::Album.create(existing_ablum).save
+    existing_album = DATA[:albums][1]
+    DFans::Album.create(existing_album).save
     id = DFans::Album.first.id
 
     get "/api/v1/albums/#{id}"
@@ -30,7 +30,7 @@ describe 'Test Album Handling' do
 
     result = JSON.parse last_response.body
     _(result['data']['attributes']['id']).must_equal id
-    _(result['data']['attributes']['name']).must_equal existing_ablum['name']
+    _(result['data']['attributes']['name']).must_equal existing_album['name']
   end
 
   it 'SAD: should return error if unknown album requested' do
@@ -40,17 +40,17 @@ describe 'Test Album Handling' do
   end
 
   it 'HAPPY: should be able to create new albums' do
-    existing_ablum = DATA[:albums][1]
+    existing_album = DATA[:albums][1]
 
     req_header = { 'CONTENT_TYPE' => 'application/json' }
-    post 'api/v1/albums', existing_ablum.to_json, req_header
+    post 'api/v1/albums', existing_album.to_json, req_header
     _(last_response.status).must_equal 201
     _(last_response.header['Location'].size).must_be :>, 0
 
     created = JSON.parse(last_response.body)['data']['data']['attributes']
-    proj = DFans::Album.first
+    album = DFans::Album.first
 
-    _(created['id']).must_equal proj.id
-    _(created['name']).must_equal existing_ablum['name']
+    _(created['id']).must_equal album.id
+    _(created['name']).must_equal existing_album['name']
   end
 end
