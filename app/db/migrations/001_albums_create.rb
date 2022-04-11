976acff543d266a54155fa08ed905d19211bf8d5
Huang Chuan <river1440>
2022-04-11 15:14:01 +0800

    set environment, create database.

---
+++ app/db/migrations/001_albums_create.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:albums) do
+      primary_key :id
+
+      String :name, unique: true, null: false
+      String :description
+      String [:tags]
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
---
+++ app/db/migrations/002_photos_create.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:photos) do
+      primary_key :id
+      foreign_key :album_id, table: :albums
+
+      String :filename, null: false
+      String :relative_path, null: false, default: ''
+      String :description
+
+      DateTime :created_at
+      DateTime :updated_at
+
+      unique [:album_id, :relative_path, :filename]
+    end
+  end
+end
