5b7ef419b434f2b5b47d67e15ce0e2707fc2f3be
Leo Shr <gogoloso123@gmail.com>
2022-04-28 13:55:22 +0800

    1.db/store added

--- app/services/create_album_for_owner.rb
+++ app/services/create_album_for_owner.rb
@@ -1,9 +1,9 @@
 module DFans
-  # Service object to create a new project for an owner
+  # Service object to create a new Album for an owner
   class CreateAlbumForOwner
     def self.call(owner_id:, album_data:)
       Account.find(id: owner_id)
-             .add_owned_project(album_data)
+             .add_owned_album(album_data)
     end
   end
 end
