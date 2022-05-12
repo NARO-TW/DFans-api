e7a64994a8e5584b7dd1c44ecd8fd776b58430a5
Leo Shr <gogoloso123@gmail.com>
2022-05-12 19:53:47 +0800

    rubocop improved

--- config/environments.rb
+++ config/environments.rb
@@ -19,28 +19,23 @@ module DFans
         path: File.expand_path('config/secrets.yml')
       )
       Figaro.load
-
       # Make the environment variables accessible to other classes
-      def self.config
-        Figaro.env
-      end
+      def self.config = Figaro.env
 
+      # Database Setup
       # Connect and make the database accessible to other classes
       db_url = ENV.delete('DATABASE_URL')
       DB = Sequel.connect("#{db_url}?encoding=utf8")
-      def self.DB
-        DB
-        # rubocop:disable Naming/MethodName
-      end
+      def self.DB = DB # rubocop:disable Naming/MethodName
 
+      # Logger setup
       LOGGER = Logger.new($stderr)
-      def self.logger
-        LOGGER
-      end
+      def self.logger = LOGGER
 
       # Load crypto keys
       SecureDB.setup(ENV.delete('DB_KEY'))
     end
+    # rubocop:enable Lint/ConstantDefinitionInBlock
 
     configure :development, :test do
       require 'pry'
