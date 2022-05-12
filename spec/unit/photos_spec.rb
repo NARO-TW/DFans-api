fa22d8cd3e5867f60e765bea4eb912a0012eb0cc
Leo Shr <gogoloso123@gmail.com>
2022-05-12 20:23:36 +0800

    rubocop improved & debug

--- spec/unit/photos_spec.rb
+++ spec/unit/photos_spec.rb
@@ -4,7 +4,6 @@ require_relative '../spec_helper'
 
 describe 'Test Photo Handling' do
   include Rack::Test::Methods
-
   before do
     wipe_database
 
@@ -15,29 +14,37 @@ describe 'Test Photo Handling' do
 
   it 'HAPPY: should retrieve correct data from database' do
     pho_data = DATA[:photos][1]
-    album = DFans::Album.first
-    new_pho = album.add_photo(pho_data)
+    alb = DFans::Album.first
+    new_pho = alb.add_photo(pho_data)
 
     pho = DFans::Photo.find(id: new_pho.id)
     _(pho.filename).must_equal pho_data['filename']
     _(pho.description).must_equal pho_data['description']
   end
 
-  it 'SECURITY: should secure sensitive attributes' do
-    pho_data = DATA[:photos][1]
-    album = DFans::Album.first
-    new_pho = album.add_photo(pho_data)
-    stored_pho = app.DB[:photos].first
+  # it 'SECURITY: should secure sensitive attributes' do
+  #   pho_data = DATA[:photos][1]
+  #   alb = DFans::Album.first
+  #   new_pho = alb.add_photo(pho_data)
+  #   stored_pho = app.DB[:photos].first
 
-    _(stored_pho[:description_secure]).wont_equal new_pho.description
+  #   _(stored_pho[:description_secure]).wont_equal new_pho.description
+  # end
+
+  it 'SECURITY: should not use deterministic integers' do
+    pho_data = DATA[:photos][1]
+    alb = DFans::Album.first
+    new_pho = alb.add_document(pho_data)
+    _(new_pho.id.is_a?(Numeric)).must_equal false
   end
 
   it 'SECURITY: should secure sensitive attributes' do
     pho_data = DATA[:photos][1]
-    album = DFans::Album.first
-    new_pho = album.add_photo(pho_data)
+    alb = DFans::Album.first
+    new_pho = alb.add_photo(pho_data)
     stored_pho = app.DB[:photos].first
 
     _(stored_pho[:description_secure]).wont_equal new_pho.description
+    _(stored_pho[:content_secure]).wont_equal new_pho.description
   end
 end
