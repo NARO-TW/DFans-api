5b483bb47f8d21da38fa64f63418377584ef39d3
Leo Shr <gogoloso123@gmail.com>
2022-05-12 20:10:43 +0800

    rubocop improved

--- spec/integration/service_add_participant_spec.rb
+++ spec/integration/service_add_participant_spec.rb
@@ -10,34 +10,29 @@ describe 'Test AddParticipantToAlbum service' do
     DATA[:accounts].each do |account_data|
       DFans::Account.create(account_data)
     end
-    
     album_data = DATA[:albums].first
-    
     @owner = DFans::Account.all[0]
     @participant = DFans::Account.all[1]
     @album = DFans::CreateAlbumForOwner.call(
-          owner_id: @owner.id, album_data: album_data
-        )
+      owner_id: @owner.id, album_data:
+    )
   end
 
-  it 'HAPPY: should be able to add a participant to a album' do 
-    DFans::AddParticipantToAlbum.call(email: @participant.email,album_id: @album.id)
-    
+  it 'HAPPY: should be able to add a participant to a album' do
+    DFans::AddParticipantToAlbum.call(
+      email: @participant.email,
+      album_id: @album.id
+    )
     _(@participant.albums.count).must_equal 1
     _(@participant.albums.first).must_equal @album
   end
 
   it 'BAD: should not add owner as a participant' do
-    _(proc{
+    _(proc {
       DFans::AddParticipantToAlbum.call(
-          email: @owner.email,
-          album_id: @album.id)
-        }).must_raise DFans::AddParticipantToAlbum::OwnerNotParticipantError
+        email: @owner.email,
+        album_id: @album.id
+      )
+    }).must_raise DFans::AddParticipantToAlbum::OwnerNotParticipantError
   end
 end
-
-
-
-
-
-
