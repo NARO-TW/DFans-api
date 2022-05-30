fb9e96809678e092ccad3265d4e03a76d6ee5c43
Leo Shr <gogoloso123@gmail.com>
2022-05-30 16:19:08 +0800

    fix: can_read & can_write scope added

--- app/policies/album_policy.rb
+++ app/policies/album_policy.rb
@@ -3,22 +3,23 @@
 module DFans
   # Policy to determine if an account can view a particular album
   class AlbumPolicy
-    def initialize(account, album)
+    def initialize(account, album, auth_scope = nil)
       @account = account
       @album = album
+      @auth_scope = auth_scope
     end
 
     def can_view?
-      account_is_owner? || account_is_participant?
+      can_read? && (account_is_owner? || account_is_participant?)
     end
 
     # duplication is ok!
     def can_edit?
-      account_is_owner? || account_is_participant?
+      can_write? && (account_is_owner? || account_is_participant?)
     end
 
     def can_delete?
-      account_is_owner?
+      can_write? && account_is_owner?
     end
 
     def can_leave?
@@ -26,26 +27,26 @@ module DFans
     end
 
     def can_add_photos?
-      account_is_owner? || account_is_participant?
+      can_write? && (account_is_owner? || account_is_participant?)
     end
 
     def can_remove_photos?
-      account_is_owner? || account_is_participant?
+      can_write? && (account_is_owner? || account_is_participant?)
     end
 
     def can_add_participants?
-      account_is_owner?
+      can_write? && account_is_owner?
     end
 
     def can_remove_participants?
-      account_is_owner?
+      can_write? && account_is_owner?
     end
 
     def can_collaborate?
-      !(account_is_owner? or account_is_participant?)
+      !(account_is_owner? || account_is_participant?)
     end
 
-    def summary
+    def summary # rubocop:disable Metrics/MethodLength
       {
         can_view: can_view?,
         can_edit: can_edit?,
@@ -60,6 +61,14 @@ module DFans
     end
 
     private
+
+    def can_read?
+      @auth_scope ? @auth_scope.can_read?('albums') : false
+    end
+
+    def can_write?
+      @auth_scope ? @auth_scope.can_write?('albums') : false
+    end
 
     def account_is_owner?
       @album.owner == @account
