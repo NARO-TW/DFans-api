b4b6857201a0748221f4afcc0b23fcbf69e90136
Leo Shr <gogoloso123@gmail.com>
2022-05-30 16:12:41 +0800

    1. auth_scope added into the 'participant_request_policy.rb' & 'photo_policy'.
    2. Moderfy the 'participant_request_policy.rb'  based on the can_write function.
    
    3. Moderfy the 'photo_policy' by adding precondition of can_read  and can_write function.

--- app/policies/participant_request_policy.rb
+++ app/policies/participant_request_policy.rb
@@ -3,24 +3,30 @@
 module DFans
     # Policy to determine if an account can view a particular album
   class ParticipationRequestPolicy
-    def initialize(album, requestor_account, target_account)
+    def initialize(album, requestor_account, target_account, auth_scope = nil)
       @Album = Album
       @requestor_account = requestor_account
       @target_account = target_account
-      @requestor = AlbumPolicy.new(requestor_account, album)
-      @target = AlbumPolicy.new(target_account, album)
+      @requestor = AlbumPolicy.new(requestor_account, album, auth_scope)
+      @target = AlbumPolicy.new(target_account, album, auth_scope)
     end
-  
+
     def can_invite?
-      @requestor.can_add_participants? && @target.can_participant?
+      can_write? &&
+      (@requestor.can_add_participants? && @target.can_participant?)
     end
-  
+
     def can_remove?
-      @requestor.can_remove_participants? && target_is_participant?
+      can_write? &&
+      (@requestor.can_remove_participants? && target_is_participant?)
     end
-  
+
     private
-  
+
+    def can_write?
+      @auth_scope ? @auth_scope.can_write?('albums') : false
+    end
+
     def target_is_participant?
       @album.participants.include?(@target_account)
     end
